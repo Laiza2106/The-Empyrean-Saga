49c7552e30971b111034ac4ea3f358e0dac16934
Laiza2106 <laiza.tavares@sptech.school>
2024-11-27 04:55:56 -0300

    Conferindo site, tudo certo

--- src/database/Script projeto individual.sql
+++ src/database/Script projeto individual.sql
@@ -1,7 +1,6 @@
-create database The_Empyrean; 
 use The_Empyrean;
 
-create table Usuario(
+create table usuario(
 idUsuario int primary key auto_increment,
 Nome varchar(45),
 Email varchar(45),
@@ -9,166 +8,55 @@ Telefone char(11),
 Senha varchar(45)
 );
 
-create table Avaliacao(
+create table livro(
+idLivro int primary key auto_increment,
+Nome varchar(45),
+dtLancamento date
+);
+
+create table avaliacao(
 idAvaliacao int primary key auto_increment,
 Nota int,
 	constraint chkNota check (Nota in (1, 2, 3, 4, 5)),
 Descricao varchar(500),
 fkUsuario int,
 	constraint fkUsuarioAvaliacao foreign key (fkUsuario)
-			references Usuario(idUsuario)
-);
-
-create table Livro(
-idLivro int primary key auto_increment,
-Nome varchar(45),
-dtLancamento date
-);
-
-create table Registro (
-idRegistro int primary key auto_increment,
+			references usuario(idUsuario),
 fkLivro int,
-	constraint fkLivroRegistro foreign key (fkLivro)
-			references Livro(idLivro),
-fkAvaliacao_Livro int,
-	constraint fkAvaliacaoRegistro foreign key (fkAvaliacao_Livro)
-			references Avaliacao(idAvaliacao)
+	constraint fkLivroAvaliacao foreign key (fkLivro)
+			references livro(idLivro)
 );
 
-insert into Usuario (Nome, Email, Telefone, Senha) values
+insert into usuario (Nome, Email, Telefone, Senha) values
 ('Laiza', 'laizatavares@gmail.com', '11993473590', 'Lol@2502'),
 ('Geovanna', 'geovanna@gmail.com', '1125674896', '@Ge19'),
 ('Amanda', 'amanda@gmail.com', 11945678765, 'Amanda123');
 
-insert into Livro (Nome, dtLancamento) values
+insert into livro (Nome, dtLancamento) values
 ('Quarta Asa', '2023-10-24'),
 ('Chama de Ferro', '2024-03-19'),
 ('Tempestade de Ônix', '2025-01-22');
 
-insert into Avaliacao (Nota, descricao, fkUsuario) values
-(5, 'Perfeito!', 1),
-(4, 'Plot twist...', 1),
-(5, 'Não vou Superar', 2),
-(5, 'Sem palavras', 2),
-(5, 'Simplesmente incrível', 3),
-(5, 'Estou surtando!!!', 3);
+insert into avaliacao (Nota, descricao, fkUsuario, fkLivro) values
+(5, 'Perfeito!', 1, 1),
+(4, 'Plot twist...', 1, 2),
+(5, 'Não vou Superar', 2, 3),
+(5, 'Sem palavras', 2, 1),
+(5, 'Simplesmente incrível', 3, 2),
+(5, 'Estou surtando!!!', 3, 3);
 
-insert into Curtidas (fkUsuarioLike, fkAvaliacao) values
-('1', '1'),
-('2', '4'),
-('3', '1'),
-('3', '2');
+select * from avaliacao;
+select * from usuario;
+select * from livro;
 
-insert into Registro (fkLivro, fkAvaliacao_Livro) values
-(1, 1),
-(2, 2),
-(1, 3),
-(2, 4);
-
-select * from Usuario;
-select * from Avaliacao;
-select * from Curtidas;
-select * from Livro;
-select * from Registro;
-
-select Usuario.Nome as 'Usuário', Usuario.Email as 'E-mail do Usuário',
-Livro.Nome as 'Nome do Livro', Avaliacao.Nota as 'Avaliação do Livro',
-Avaliacao.Descricao as 'Comentário da Leitura'
-from Usuario 
-join Avaliacao
-on Usuario.idUsuario = Avaliacao.fkUsuario
-join Registro
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro;
-
-select Usuario.Nome as 'Usuário', Livro.Nome as 'Nome do Livro', 
-Avaliacao.Descricao as 'Comentário da Leitura',
-Curtidas.Likes as 'Quantidade de Likes'
-from Usuario
-join Avaliacao
-on Usuario.idUsuario = Avaliacao.fkUsuario
-join Registro
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-join Curtidas
-on Curtidas.fkAvaliacao = Avaliacao.idAvaliacao;
-
-select Usuario.Nome as 'Usuário', Usuario.Email as 'E-mail do Usuário',
-Livro.Nome as 'Nome do Livro', Avaliacao.Nota as 'Avaliação do Livro',
-Avaliacao.Descricao as 'Comentário da Leitura'
-from Usuario 
-join Avaliacao
-on Usuario.idUsuario = Avaliacao.fkUsuario
-join Registro
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-where Usuario.idUsuario = 1;
-
-select Usuario.Nome as 'Usuário', Usuario.Email as 'E-mail do Usuário',
-Livro.Nome as 'Nome do Livro', Avaliacao.Nota as 'Avaliação do Livro',
-Avaliacao.Descricao as 'Comentário da Leitura'
-from Usuario 
-join Avaliacao
-on Usuario.idUsuario = Avaliacao.fkUsuario
-join Registro
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-where Usuario.idUsuario = 2;
-
-select Usuario.Nome as 'Usuário', Livro.Nome as 'Nome do Livro', 
-Avaliacao.Descricao as 'Comentário da Leitura',
-Curtidas.Likes as 'Quantidade de Likes'
-from Usuario
-join Avaliacao
-on Usuario.idUsuario = Avaliacao.fkUsuario
-join Registro
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-join Curtidas
-on Curtidas.fkAvaliacao = Avaliacao.idAvaliacao
-where Usuario.idUsuario = 1;
-
-select Usuario.Nome as 'Usuário', Livro.Nome as 'Nome do Livro', 
-Avaliacao.Descricao as 'Comentário da Leitura',
-Curtidas.Likes as 'Quantidade de Likes'
-from Usuario
-join Avaliacao
-on Usuario.idUsuario = Avaliacao.fkUsuario
-join Registro
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-join Curtidas
-on Curtidas.fkAvaliacao = Avaliacao.idAvaliacao
-where Usuario.idUsuario = 2;
-
-select truncate(avg(Nota), 2) as "Média" from Avaliacao;
-
-select truncate(avg(Avaliacao.Nota), 2) as "Média"
-from Avaliacao 
-join Registro 
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-where Livro.idLivro = 1;
-
-select truncate(avg(Avaliacao.Nota), 2) as "Média"
-from Avaliacao 
-join Registro 
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-where Livro.idLivro = 2;
-
-select truncate(avg(Avaliacao.Nota), 2) as "Média"
-from Avaliacao 
-join Registro 
-on Avaliacao.idAvaliacao = Registro.fkAvaliacao_Livro
-join Livro
-on Livro.idLivro = Registro.fkLivro
-where Livro.idLivro = 3;
+SELECT 
+    (SELECT COUNT(*) FROM usuario) AS Total_Usuarios,
+    l.Nome AS Livro_Mais_Avaliado,
+    ROUND(AVG(a.Nota), 2) AS Media_Avaliacoes,
+    COUNT(a.fkLivro) AS Quantidade_Avaliacoes
+    FROM livro as l
+    LEFT JOIN avaliacao as a 
+    ON l.idLivro = a.fkLivro
+    GROUP BY l.Nome
+    ORDER BY Quantidade_Avaliacoes DESC
+LIMIT 1;
